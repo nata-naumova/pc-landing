d21d276ccbf392c8aa9450cada9e64f429d7cfcc
nata-naumova <naumonata@yandex.ru>
2024-02-08 14:07:01 +0300

    update

--- src/components/WorkSteps/WorkSteps.jsx
+++ src/components/WorkSteps/WorkSteps.jsx
@@ -6,26 +6,20 @@ const WorkSteps = () => {
       <h2 className="section__title">ЭТАПЫ РАБОТЫ</h2>
       <ul className={styles.list}>
         <li className={styles.item}>
-          <svg className={styles.svg}>
-            <text x="50" y="285">1</text>
-          </svg>
+          <div className={styles.number}>1</div>
           <p className={styles.text}>
             Оставьте заявку на сайте или свяжитесь с нашим менеджером.
           </p>
         </li>
         <li className={styles.item}>
-          <svg className={styles.svg}>
-            <text x="50" y="285">2</text>
-          </svg>
+          <div className={styles.number}>2</div>
           <p className={styles.text}>
             Подробно обсудим ход выполнения сборки и подберем необходимые
             комплектующие под ваш запрос.
           </p>
         </li>
         <li className={styles.item}>
-          <svg className={styles.svg}>
-            <text x="50" y="283">3</text>
-          </svg>
+          <div className={styles.number}>3</div>
           <p className={styles.text}>
             Собирем ПК вашей мечты, сделаем все необходимые тесты, бережно
             упакуем и доставим.
